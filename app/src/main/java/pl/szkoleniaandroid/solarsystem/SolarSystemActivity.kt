21a7f455fdd5639ef6631278c2399bae3667dbd2
mic1410 <mic1410@gmail.com>
2019-10-22 11:47:19 +0200

    add navigation, app is working now

---
+++ app/src/main/java/pl/szkoleniaandroid/solarsystem/SolarSystemActivity.kt
@@ -0,0 +1,26 @@
+package pl.szkoleniaandroid.solarsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.solar_system_activity.*
+
+class SolarSystemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.solar_system_activity)
+
+
+        setSupportActionBar(toolbar)
+
+        //me: extension function
+        val navController = findNavController(R.id.nav_host_fragment)
+
+//        NavigationUI.setupWithNavController(toolbar, navController)
+
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
+
+    }
+}
